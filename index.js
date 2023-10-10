5c476983df1e2c36b1774ffb620228100dc20c88
Nicholus <nicholus.muriithi@student.moringaschool.com>
2023-10-10 15:29:18 -0700

    commit solves assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your code in this file!
+//Assignment on Strings Lab...
+//Project Flatbook--Flatiron School-based social network
+//first method uses string concatenation(link) using the (+) operator
+const currentUser = 'Taxi Driver';
+const welcomeMessage = 'Welcome to Flatbook, ' + currentUser + '!'
+console.log(welcomeMessage)
+//Second method uses String interpolation using Template literals 
+//solution below also has desired result.
+// const currentUser = 'Taxi Driver';
+// const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
